netfab/libg15

commit 4d1d5b47b461f6d0533d9ccce9d569a3fab76f87

Fix warning.
 libg15.c:835:9: error: suggest parentheses around operand of '!' or
   change '&' to '&&' or '!' to '~' [-Werror=parentheses]
     if((!g15DeviceCapabilities() & G15_DEVICE_COLOUR))

libg15.c
@@ -832,7 +832,7 @@ int setKBBrightness(unsigned int level)
 
 int setG510LEDColor(unsigned char r, unsigned char g, unsigned char b)
 {
-    if((!g15DeviceCapabilities() & G15_DEVICE_COLOUR))
+    if( ( ! g15DeviceCapabilities() ) & G15_DEVICE_COLOUR )
         return G15_ERROR_UNSUPPORTED;
 
     int retval = 0;

commit 76a0b7c542968c2e44f66949a4c55af6a3f3cc1e

Fix regression introduced in commit 4d1d5b4.
Logical not must be performed on the whole expression.

libg15.c
@@ -833,7 +833,7 @@ int setKBBrightness(unsigned int level)
 
 int setG510LEDColor(unsigned char r, unsigned char g, unsigned char b)
 {
-    if( ( ! g15DeviceCapabilities() ) & G15_DEVICE_COLOUR )
+    if( ! (g15DeviceCapabilities()&G15_DEVICE_COLOUR))
         return G15_ERROR_UNSUPPORTED;
 
     int retval = 0;